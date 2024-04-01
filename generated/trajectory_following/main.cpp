1cd250ce7ea060b8d4caaca720b7bdf4f5822780
kyoichi-sugahara <kyoichi.sugahara@tier4.jp>
2024-04-01 15:10:10 +0900

    temp
    Signed-off-by: kyoichi-sugahara <kyoichi.sugahara@tier4.jp>

--- generated/trajectory_following/main.cpp
+++ generated/trajectory_following/main.cpp
@@ -3,12 +3,12 @@
 // (https://github.com/ohtsukalab/autogenu-jupyter). The autogenu-jupyter
 // copyright holders make no ownership claim of its contents.
 
-#include "cgmres/integrator.hpp"
-#include "cgmres/logger.hpp"
 #include "cgmres/single_shooting_cgmres_solver.hpp"
 #include "cgmres/zero_horizon_ocp_solver.hpp"
 #include "ocp.hpp"
 
+#include "cgmres/integrator.hpp"
+#include "cgmres/logger.hpp"
 #include <string>
 
 int main() {
@@ -32,8 +32,7 @@ int main() {
   // Define the initial time and initial state.
   const double t0 = 0;
   cgmres::Vector<3> x0;
-  // x0 << 0, 0, 0;
-  x0 << 0.018, 0.151069, 0;
+  x0 << 0, 0, 0;
 
   // Initialize the solution of the C/GMRES method.
   constexpr int kmax_init = 1;
@@ -43,9 +42,6 @@ int main() {
   uc0 << 0.0;
   initializer.set_uc(uc0);
   initializer.solve(t0, x0);
-  std::cerr << "u =: " << initializer.ucopt()[0] << std::endl;
-
-  std::cerr << "u = : " << initializer.uopt() << std::endl;
 
   // Define the C/GMRES solver.
   constexpr int N = 50;
@@ -56,7 +52,7 @@ int main() {
   mpc.init_dummy_mu();
 
   // Perform a numerical simulation.
-  const double tsim = 1;
+  const double tsim = 100;
   const double sampling_time = settings.sampling_time;
   const unsigned int sim_steps = std::floor(tsim / sampling_time);
 
@@ -69,18 +65,17 @@ int main() {
 
   std::cout << "Start a simulation..." << std::endl;
   for (unsigned int i = 0; i < sim_steps; ++i) {
-    // std::cerr << "sim_steps: " << i << std::endl;
+    std::cerr << "sim_steps: " << i << std::endl;
     const auto &u =
         mpc.uopt()[0]; // const reference to the initial optimal control input
-    // const cgmres::VectorX x1 = cgmres::RK4(ocp, t, sampling_time, x, u);  //
-    // the next state
-    mpc.update(t, x); // update the MPC solution
+    const cgmres::VectorX x1 =
+        cgmres::RK4(ocp, t, sampling_time, x, u); // the next state
+    mpc.update(t, x);                             // update the MPC solution
 
     mpc.optError(t, x, settings.verbose_level); // compute the optimal error
     logger.save(t, x, u, mpc.uopt(), mpc.optError(), mpc.normDiff(),
                 mpc.StandardDeviation());
-    // x = x1;
-
+    x = x1;
     t = t + sampling_time;
   }
   std::cout << "End the simulation" << std::endl;
