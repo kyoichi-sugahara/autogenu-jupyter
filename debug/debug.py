5b3de29ef389f312761c36ab034749a32795a842
kyoichi-sugahara <kyoichi.sugahara@tier4.jp>
2024-05-23 14:56:31 +0900

    update debug script
    Signed-off-by: kyoichi-sugahara <kyoichi.sugahara@tier4.jp>

--- debug/debug.py
+++ debug/debug.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import argparse
 import matplotlib.pyplot as plt
 import numpy as np
 from plotter import Plotter
@@ -21,7 +22,9 @@ import visualize_path
 from visualize_path import plot_trajectory
 
 
-def main():
+def main(
+    show_plotter=True, show_animation=True, show_images=True, show_trajectory=True
+):
     home_directory = os.path.expanduser("~")
     cgmres_directory_path = os.path.join(home_directory, ".ros/log")
 
@@ -48,46 +51,87 @@ def main():
 
     plotter = Plotter(log_dir=latest_cgmres_directory)
     plotter.set_scales(2, 5, 2)
-    plotter.show()
-    # plotter.save()
-    plt.draw()
-    plt.pause(1)
+    if show_plotter:
+        plotter.show()
+        # plotter.save()
+        plt.draw()
+        plt.pause(1)
+    else:
+        plotter.save()
 
     anim = TrajectoryFollowing(
         cgmres_log_dir=latest_cgmres_directory,
         trajectory_log_dir=latest_trajectory_directory,
     )
     # anim.set_skip_frames(10)
-    anim.generate_animation()
+    if show_animation:
+        anim.generate_animation()
 
-    plots_dir = os.path.join(latest_cgmres_directory, "plots")
-    image_files = sorted([f for f in os.listdir(plots_dir) if f.endswith(".png")])
-    first_index = 0
-    last_index = len(image_files) - 1
-    num_images = 10
-    indices = np.linspace(first_index, last_index, num_images, dtype=int)
-    selected_images = [image_files[i] for i in indices]
+    if show_images:
+        plots_dir = os.path.join(latest_cgmres_directory, "plots")
+        image_files = sorted([f for f in os.listdir(plots_dir) if f.endswith(".png")])
+        first_index = 0
+        last_index = len(image_files) - 1
+        num_images = 10
+        indices = np.linspace(first_index, last_index, num_images, dtype=int)
+        selected_images = [image_files[i] for i in indices]
 
-    fig, axes = plt.subplots(5, 2, figsize=(10, 20))
-    axes = axes.ravel()
-    for i, image_file in enumerate(selected_images):
-        img = plt.imread(os.path.join(plots_dir, image_file))
-        axes[i].imshow(img)
-        axes[i].axis("off")
-        axes[i].set_title(f"Frame {os.path.splitext(image_file)[0][6:]}")
+        fig, axes = plt.subplots(5, 2, figsize=(10, 20))
+        axes = axes.ravel()
+        for i, image_file in enumerate(selected_images):
+            img = plt.imread(os.path.join(plots_dir, image_file))
+            axes[i].imshow(img)
+            axes[i].axis("off")
+            axes[i].set_title(f"Frame {os.path.splitext(image_file)[0][6:]}")
 
-    plt.tight_layout()
-    plt.draw()
-    plt.pause(1)
+        plt.tight_layout()
+        plt.draw()
+        plt.pause(1)
 
-    plot_trajectory()
-    plt.draw()
-    plt.pause(1)
+    if show_trajectory:
+        plot_trajectory()
+        plt.draw()
+        plt.pause(1)
 
-    plt.show(block=False)
+    if show_plotter or show_animation or show_images or show_trajectory:
+        plt.show(block=False)
 
-    input("Press Enter to continue and close plots...")
+        input("Press Enter to continue and close plots...")
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description="Process and plot CGMRES and trajectory data."
+    )
+    parser.add_argument(
+        "--plotter",
+        action=argparse.BooleanOptionalAction,
+        default=True,
+        help="Show plotter plots.",
+    )
+    parser.add_argument(
+        "--animation",
+        action=argparse.BooleanOptionalAction,
+        default=True,
+        help="Generate animation.",
+    )
+    parser.add_argument(
+        "--images",
+        action=argparse.BooleanOptionalAction,
+        default=True,
+        help="Show image plots.",
+    )
+    parser.add_argument(
+        "--trajectory",
+        action=argparse.BooleanOptionalAction,
+        default=True,
+        help="Show trajectory plots.",
+    )
+    args = parser.parse_args()
+
+    main(
+        show_plotter=args.plotter,
+        show_animation=args.animation,
+        show_images=args.images,
+        show_trajectory=args.trajectory,
+    )
